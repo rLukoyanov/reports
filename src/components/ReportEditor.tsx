d7dc878e95e899a3c99847a6921fcd33f5574809
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-07-15 22:48:00 +0300

    add image uploader

--- src/components/ReportEditor.tsx
+++ src/components/ReportEditor.tsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 import styles from "./ReportEditor.module.css";
 import PocketBase from "pocketbase";
+import ReactQuill, { Quill } from "react-quill";
+// import ImageUploader from "quill-image-uploader";
+
+import 'quill-image-uploader/dist/quill.imageUploader.min.css';
 
 const pb = new PocketBase("https://theaesthetics.ru");
 
 // Dynamic import to avoid SSR issues with react-quill
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+// Add ImageUploader module
+// Quill.register("modules/imageUploader", ImageUploader);
 
 const ReportEditor: React.FC = () => {
   const [report, setReport] = useState<string>("");
   const [reports, setReports] = useState<string[]>([]);
-
-  const handleSaveReport = () => {
-    setReports([...reports, report]);
-    setReport("");
-  };
 
   const handleSubmit = async () => {
     try {
@@ -32,12 +32,46 @@ const ReportEditor: React.FC = () => {
     }
   };
 
+  const uploadImage = async (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await pb.collection("uploads").create(formData);
+    return response.file.url; // Предполагается, что PB возвращает URL файла
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+    // imageUploader: {
+    //   upload: uploadImage,
+    // },
+  };
+
+  const handleChange = (value: string) => {
+    console.log("Отчет:", value); // Проверка значения
+    setReport(value);
+  };
+  
+
   return (
     <div className={styles.editorContainer}>
       <ReactQuill
         value={report}
-        onChange={setReport}
+        onChange={handleChange}
         className={styles.quillEditor}
+        modules={modules}
       />
       <button onClick={handleSubmit} className={styles.saveButton}>
         Сохранить Отчет
